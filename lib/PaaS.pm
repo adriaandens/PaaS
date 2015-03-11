630f81e9edc7ec5f164fafa0eec9e58884055097
Adriaan Dens <adriaan.dens@gmail.com>
2015-03-11 15:44:31 +0100

    Implemented some functions

--- lib/PaaS.pm
+++ lib/PaaS.pm
@@ -21,10 +21,10 @@ sub run {
     my $printjob_data = get_printjob_data($datafile);
 
     # Parse Tea4cups job options
-    my %parsed_options = parse_options($options);
+    my $parsed_options = parse_options($options);
 
     # Merge tea4cups options and printjob hash
-    merge_hashes($printjob_data, %parsed_options); # Merged into first argument
+    merge_hashes($printjob_data, $parsed_options); # Merged into first argument
 
     # Get all printers which are capable of doing the printjob
     my %capable_printers = get_capable_printers(%printjob_data);
@@ -56,12 +56,16 @@ sub get_user_data {}
 
 sub get_working_printers {}
 
+# INPUT: The filename which contains the printjob
+# OUTPUT: a reference to a hash containing printjob key value pairs
 sub get_printjob_data {
     my $datafile = shift;
     print "Datafile is: $datafile\n";
     return PaaS::PrintjobParser::parse_printjob($datafile);
 }
 
+# INPUT: a string containing options
+# OUTPUT: a reference to a hash containing the options from the string
 sub parse_options {
     # Example: finishings=3 number-up=1 job-uuid=urn:uuid:4041cb3c-b055-38f8-6f05-f0c2a76f2f81 job-originating-host-name=localhost time-at-creation=1425393352 time-at-processing=1425393352
     my %options = ();
@@ -73,7 +77,14 @@ sub parse_options {
     return \%options;
 }
 
-sub merge_hashes {}
+# INPUT: Two hashes
+# OUTPUT: None
+sub merge_hashes {
+    my ($hash1, $hash2) = @_;
+    foreach my $key (keys %$hash2) {
+        $$hash1{$key} = $$hash2{$key};
+    }
+}
 
 sub get_capable_printers {}
 
@@ -82,6 +93,11 @@ sub sort_printers {}
 # INPUT: Name of the file containing the printjob, and to which printer (the name of) to print
 # OUTPUT: Result code of print job
 sub print_job {
+    my ($datafile, $printername) = @_;
+
+    # Just run it :3
+    `/usr/bin/lpr -P $printername $datafile`;
+
     return 0;
 }
 
