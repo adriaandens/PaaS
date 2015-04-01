06a2624c33fe801aa17254bb3ecb3529af5acdc9
Adriaan Dens <adriaan.dens@gmail.com>
2015-04-01 15:25:18 +0200

    implemented monitoring

--- lib/PaaS/Monitoring.pm
+++ lib/PaaS/Monitoring.pm
@@ -4,10 +4,26 @@ use PaaS;
 use Exporter qw(import);
 
 our $VERSION = '0.1';
-our @EXPORT = qw();
+our @EXPORT = qw(monitor_printers);
 
 sub monitor_printers {
     # Use IPPtool to get state of printers
-    # Use IPPtool to get queue
+    foreach my $printer (keys %$printers) {
+        my $ip = ${$printers}{$printer}->{ipaddr};
+        my $output = `ipptool ipp://$ip/printers/$printer /usr/share/cups/ipptool/get-jobs.test`;
+
+        my @lines = split(/\n/, $output);
+        my ($jobid, $state) = ('', '');
+        foreach(@lines) {
+            if(/^(\d+)\s+([\w-]+)/) {
+                $jobid = $1; $state = $2;
+            }
+        }
+
         # Change jobstate accordingly
+        set_jobstate($jobid, $state);
+    }
+
+    # Looks like:
+    # job-id job-state job-name job-originating-user-name job-impressions job-impressions-completed job-media-sheets job-media-sheets-completed
 }
